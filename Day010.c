98e93a17e7a4e03cd3ac5602237a1ce3dc8df8f1
yugmakani77 <yugmakani675@gmail.com>
2026-02-15 00:47:34 +0530

    Update and rename Day10.c to Day010.c

--- Day010.c
+++ Day010.c
@@ -1,19 +1,5 @@
 // Problem: Read a string and check if it is a palindrome using two-pointer comparison.
 
-// Input:
-// - Single line: string s
-
-// Output:
-// - Print YES if palindrome, otherwise NO
-
-// Example:
-// Input:
-// level
-
-// Output:
-// YES
-
-// Explanation: String reads same forwards and backwards
 #include <stdio.h>
 #include <string.h>
 
@@ -41,3 +27,4 @@ int main() {
 
     return 0;
 }
+
