67098a8ec3affa3165e22dd0fa198f199ecbd29c
yugmakani77 <yugmakani675@gmail.com>
2026-02-18 11:58:07 +0530

    Add files via upload

---
+++ Day017.c
@@ -0,0 +1,31 @@
+/*Write a program to find the maximum and minimum values present in a given array of integers.*/
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int max = arr[0];
+    int min = arr[0];
+
+    for(int i = 1; i < n; i++) {
+        if(arr[i] > max) {
+            max = arr[i];
+        }
+        if(arr[i] < min) {
+            min = arr[i];
+        }
+    }
+
+    printf("Max: %d\n", max);
+    printf("Min: %d\n", min);
+
+    return 0;
+}
